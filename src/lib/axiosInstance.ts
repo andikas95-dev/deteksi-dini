093efe860957376302a4d093fd316accc150576f
andikas95-dev <andikaperdana95@gmail.com>
2025-07-15 14:27:00 +0700

    fix: increase axios timeout from 3000ms to 10000ms for improved request handling

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -4,6 +4,6 @@ const baseURL = process.env.NEXTAUTH_URL || 'https://deteksi-dini.vercel.app';
 
 export const locbe = axios.create({
   baseURL: `${baseURL}/api`,
-  timeout: 3000,
+  timeout: 10000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
